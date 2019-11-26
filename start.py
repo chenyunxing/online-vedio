ac333dcfd2089cae7bd0d6970b39e2a7e4fd6526
chenyunxing <807385017@qq.com>
2019-11-27 00:30:09 +0800

    增加检测端口，然后再指定端口启动的功能

--- start.py
+++ start.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 import os
+import socket
 app = Flask(__name__)
 
 # dir = 'C:\\Users\\yinxixing'
@@ -20,14 +21,27 @@ def test(name=None,dirname='videos'):
     dirname = 'game'
     return render_template('hello.html', name=name,dirname=dirname)
 
-if __name__ == '__main__':
-    # 检查端口占用
-    import socket
+# 检查端口找到可用端口开启服务
+def checkport():
     ip = '127.0.0.1'
     port = 8080
-    socket.setdefaulttimeout(3)
+    socket.setdefaulttimeout(1)
     s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     result=s.connect_ex((ip, port))
-    if result==0:
-        print(str(ip,u':',str(port),u'端口已占用')
-    app.run(host='0.0.0.0', port=8080,debug=True)
+    myname = socket.getfqdn(socket.gethostname())
+    myaddr = socket.gethostbyname(myname)
+    ip = myaddr
+    if result == 0:
+        while result==0:
+            port = port + 1
+            print(port)
+            result=s.connect_ex((ip, port))
+            if port == 60000:
+                print('too many port used')
+                return -1
+    return ip,port
+
+if __name__ == '__main__':
+    # 检查端口占用
+    ip,port = checkport()
+    app.run(host='0.0.0.0',port=port,debug=True)
